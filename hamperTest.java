3b6e6cf502ed33d51ccbfe23d314999c9a9f6033
WillPerks <william.perks@ucalgary.ca>
2022-03-23 14:52:53 -0600

    Added test files for Hamper class

---
+++ hamperTest.java
@@ -0,0 +1,25 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+	
+	public Tests() {
+	}
+	
+	@Test 
+	public void testGetClients() {
+		
+	}
+	
+	@Test
+	public void testHamper() {
+		
+	}
+	/*
+	 * Write tests for: Hamper
+	 */
+}
+
+
