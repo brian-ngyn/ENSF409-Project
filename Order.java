fe59a43ba3d7e6a20334c766c6dbfe87a72c456b
Dimitar Janevski <dimitar.j27@gmail.com>
2022-04-06 14:33:49 -0600

    Finished Order class

---
+++ Order.java
@@ -0,0 +1,97 @@
+package edu.ucalgary.ensf409;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Order implements PrintOutput{
+    private ArrayList<Hamper> hampers;
+    private ArrayList<String> hamperOutputs;
+    
+    /**
+     * Initialize a new Order with a list of hampers
+     * @param hamperArray
+     */
+    public Order(ArrayList<Hamper> hamperArray){
+        this.hampers = hamperArray;
+    }
+
+    /**
+     * Getter method for hampers stored in order
+     * @return hampers
+     */
+    public ArrayList<Hamper> getHampers(){
+        return this.hampers;
+    }
+
+    /**
+     * Add a new hamper to existing order
+     * @param hamper
+     */
+    public void addHamper(Hamper hamper){
+        this.hampers.add(hamper);
+    }
+
+    /**
+     * Verify that all hampers in order can be built. If a hamper cannot be built,
+     * print out an error message, otherwise, output the order form 
+     */
+    public void validateOrder(){
+        boolean valid = true;
+        for (Hamper hamper: hampers){
+            if (!hamper.validateOrder()){
+                valid = false;
+                break;
+            }
+        }
+        if (valid){
+            printOrderForm();
+        } else {
+            System.out.println("Invalid hamper configuration. Please try again.");
+        }
+    }
+
+    /**
+     * Prints out the order form for a successful order. Includes a title, and summary and item details for each hamper.
+     */
+    public void printOrderForm(){
+        try {
+            FileWriter writer = new FileWriter("filename.txt");
+            writer.write("Group 14 Food Bank\n");
+            writer.write("Hamper Order Form\n");
+            writer.write("=================================\n");
+            writer.write("\n");
+            writer.write("Original Request:\n");
+            // Write hamper summary to file
+            for (int i = 0; i < hampers.size(); i++){
+                writer.write("Hamper " + i + "\n");
+                writer.write("--------------------\n")
+                if (hampers[i].getNumAdultsM() > 0){
+                    writer.write(hampers[i].getNumAdultsM() + " Adult Males\n");
+                }
+                if (hampers[i].g2etNumAdultsF() > 0){
+                    writer.write(hampers[i].getNumAdultsF() + " Adult Females\n");
+                }
+                if (hampers[i].getNumChildUnder8() > 0){
+                    writer.write(hampers[i].getNumChildUnder8() + " Children Under 8\n");
+                }
+                if (hampers[i].getNumChildOver8() > 0){
+                    writer.write(hampers[i].getNumChildOver8() + " Children Over 8\n");
+                }
+                writer.write("\n");
+            }
+            // Write hamper details to file
+            for (int i = 0; i < hampers.size(); i++){
+                writer.write("Hamper " + i + " items:\n");
+                FoodItem[] foodItems = hampers[i].getFoodItems();
+                for (FoodItem foodItem : foodItems){
+                    writer.write(String.format("%-15f %s\n", foodItem.getItemId(), foodItem.getItemName()));
+                }
+                writer.write("\n");
+            }
+            writer.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+    }
+}
