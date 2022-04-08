e27dbecf5052be173ea254214e4696544f9b9178
brian-ngyn <brian.nguyen3@ucalgary.ca>
2022-04-08 14:03:00 -0600

    Add buttons

--- edu/ucalgary/ensf409/GUI.java
+++ edu/ucalgary/ensf409/GUI.java
@@ -73,24 +73,80 @@ class HamperGUI extends Frame{
         });
 
         maleAdultsDecrement.addActionListener(maleAdultsActionEvent -> {
-            maleAdultsCount--;
+            if (maleAdultsCount > 0){
+                maleAdultsCount--;
+            }
             maleAdultsCountLabel.setText("" + maleAdultsCount);
         });
 
         // Female adults sub-panel
         JPanel femaleAdultsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel femaleAdultsLabel = new JLabel("Number of Female Adults: " + femaleAdultsCount);
+        JLabel femaleAdultsLabel = new JLabel("Number of Female Adults: ");
+        JLabel femaleAdultsCountLabel = new JLabel("" + femaleAdultsCount);
+        JButton femaleAdultsDecrement = new JButton("-");
+        JButton femaleAdultsIncrement = new JButton("+");
         femaleAdultsPanel.add(femaleAdultsLabel);
+        femaleAdultsPanel.add(femaleAdultsDecrement);
+        femaleAdultsPanel.add(femaleAdultsCountLabel);
+        femaleAdultsPanel.add(femaleAdultsIncrement);
+
+        femaleAdultsIncrement.addActionListener(femaleAdultsActionEvent -> {
+            femaleAdultsCount++;
+            femaleAdultsCountLabel.setText("" + femaleAdultsCount);
+        });
+
+        femaleAdultsDecrement.addActionListener(femaleAdultsActionEvent -> {
+            if (femaleAdultsCount > 0){
+                femaleAdultsCount--;
+            }
+            femaleAdultsCountLabel.setText("" + femaleAdultsCount);
+        });
 
         // Child under 8 sub-panel
         JPanel childUnder8Panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel childUnder8Label = new JLabel("Number of Childen under 8: " + childUnder8Count);
+        JLabel childUnder8Label = new JLabel("Number of Childen under 8: ");
+        JLabel childUnder8CountLabel = new JLabel("" + childUnder8Count);
+        JButton childUnder8Decrement = new JButton("-");
+        JButton childUnder8Increment = new JButton("+");
         childUnder8Panel.add(childUnder8Label);
+        childUnder8Panel.add(childUnder8Decrement);
+        childUnder8Panel.add(childUnder8CountLabel);
+        childUnder8Panel.add(childUnder8Increment);
+
+        childUnder8Increment.addActionListener(childUnder8ActionEvent -> {
+            childUnder8Count++;
+            childUnder8CountLabel.setText("" + childUnder8Count);
+        });
+
+        childUnder8Decrement.addActionListener(childUnder8ActionEvent -> {
+            if (childUnder8Count > 0){
+                childUnder8Count--;
+            }
+            childUnder8CountLabel.setText("" + childUnder8Count);
+        });
 
         // Child over 8 sub-panel
         JPanel childOver8Panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel childOver8Label = new JLabel("Number of Children over 8:" + childOver8Count);
+        JLabel childOver8Label = new JLabel("Number of Children over 8: ");
+        JLabel childOver8CountLabel = new JLabel("" + childOver8Count);
+        JButton childOver8Decrement = new JButton("-");
+        JButton childOver8Increment = new JButton("+");
         childOver8Panel.add(childOver8Label);
+        childOver8Panel.add(childOver8Decrement);
+        childOver8Panel.add(childOver8CountLabel);
+        childOver8Panel.add(childOver8Increment);
+
+        childOver8Increment.addActionListener(childOver8ActionEvent -> {
+            childOver8Count++;
+            childOver8CountLabel.setText("" + childOver8Count);
+        });
+
+        childOver8Decrement.addActionListener(childOver8ActionEvent -> {
+            if (childOver8Count > 0){
+                childOver8Count--;
+            }
+            childOver8CountLabel.setText("" + childOver8Count);
+        });
 
         newHamperPanel.add(maleAdultsPanel);
         newHamperPanel.add(femaleAdultsPanel);
