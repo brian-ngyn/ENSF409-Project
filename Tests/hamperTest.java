ae8416cd4c233ab9d0f2adbfa8ed37bf087c8c83
brian-ngyn <brian.nguyen3@ucalgary.ca>
2022-04-16 19:49:27 -0600

    Hamper Test

--- Tests/hamperTest.java
+++ Tests/hamperTest.java
@@ -1,7 +1,9 @@
 package edu.ucalgary.ensf409;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
 
 public class Tests {
 	public static final int expectedAdultM = 1;
@@ -14,22 +16,23 @@ public class Tests {
 	public static final int requiredProtein = 500;
 	public static final int requiredOther = 700;
 	public static final int requiredCalories = 5000;
-	public static final int quntity = 1;
 	public static final FoodItem [] foodItems = new FoodItem();
 	
 	public static int expectedITEMID = 10;
 	public static String expectedITEMNAME = "Apples";
 	public static int expectedGRAINCONTENT = 10;
-	public static int expectedFIBERCONTENT = 15;
+	public static int expectedFRUITVEGGIESCONTENT = 15;
 	public static int expectedPROTIENCONTENT = 20;
 	public static int expectedOTHERNUTRITION = 25;
 	public static int expectedCALORIES = 500;
 	
 	public static Hamper testHamper = new Hamper(1, 1, 2, 1, 3);
-	public static FoodItem foodObj = new FoodItem(expectedITEMID, expectedITEMNAME, expectedGRAINCONTENT, expectedFIBERCONTENT, expectedPROTIENCONTENT, expectedOTHERNUTRITION, expectedCALORIES);
+	public static FoodItem foodObj = new FoodItem(expectedITEMID, expectedITEMNAME, expectedGRAINCONTENT, expectedFRUITVEGGIESCONTENT, expectedPROTIENCONTENT, expectedOTHERNUTRITION, expectedCALORIES);
 	testHamper.getFoodItems().add(foodObj);
 	
-	
+	/*
+	 * This test makes sure that Hamper returns the correct number of hampers which is 3
+	 */
 	@Test
 	public void testGetNumberOfHampers() {
 		System.out.println("Test get number of hampers");
@@ -37,6 +40,9 @@ public class Tests {
 		assertEquals("getNumberOfHampers was incorrect: ", numOfHampers, expectedquantity);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct number of Adult Males which is 1
+	 */
 	@Test
 	public void testGetNumberOfAdultM() {
 		System.out.println("Test get number of adult males");
@@ -44,6 +50,9 @@ public class Tests {
 		assertEquals("getNumAdultsM was incorrect: ", numAdultM, expectedquantity);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct number of Adult Females which is 1
+	 */
 	@Test
 	public void testGetNumberOfAdultF() {
 		System.out.println("Test get number of adult females");
@@ -51,6 +60,9 @@ public class Tests {
 		assertEquals("getNumAdultsF was incorrect: ", numAdultF, expectedquantity);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct number of children under 8 which is 1
+	 */
 	@Test
 	public void testGetNumberOfChildUnder8() {
 		System.out.println("Test get number of children under 8");
@@ -58,6 +70,9 @@ public class Tests {
 		assertEquals("getNumChildUnder8 was incorrect: ", numChildU8, expectedquantity);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct number of children over 8 which is 2
+	 */
 	@Test
 	public void testGetNumberOfChildOver8() {
 		System.out.println("Test get number of children over 8");
@@ -65,6 +80,9 @@ public class Tests {
 		assertEquals("getNumChildOver8 was incorrect: ", numChildO8, expectedquantity);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct total whole grain which is 100
+	 */
 	@Test 
 	public void testGetTotalWholeGrains() {
 		System.out.println("Test getTotalWholeGrains");
@@ -72,6 +90,9 @@ public class Tests {
 		assertEquals("getNumChildOver8 was incorrect: ", actualWholeGrains, requiredWholeGrains);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct total fruit/veggies which is 200
+	 */
 	@Test 
 	public void testGetTotalFruitVeggies() {
 		System.out.println("Test getTotalFruitVeggies");
@@ -79,6 +100,9 @@ public class Tests {
 		assertEquals("getNumChildOver8 was incorrect: ", actualFruitVeggies, requiredFruitVeggies);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct total protein which is 500
+	 */
 	@Test 
 	public void testGetTotalProtein() {
 		System.out.println("Test getTotalProtein");
@@ -86,6 +110,9 @@ public class Tests {
 		assertEquals("getNumChildOver8 was incorrect: ", actualProtein, requiredProtein);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct total other which is 700
+	 */
 	@Test 
 	public void testGetTotalOther() {
 		System.out.println("Test getTotalOther");
@@ -93,6 +120,9 @@ public class Tests {
 		assertEquals("getNumChildOver8 was incorrect: ", actualOther, requiredOther);
 	}
 	
+	/*
+	 * This test makes sure that Hamper returns the correct number of calories which is 5000
+	 */
 	@Test 
 	public void testGetTotalCalories() {
 		System.out.println("Test getTotalCalories");
@@ -100,30 +130,70 @@ public class Tests {
 		assertEquals("getNumChildOver8 was incorrect: ", actualCalories, requiredCalories);
 	}
 	
+	/*
+	 * This test makes sure that createHamper adds enough items to meet the requirements which it should meet
+	 */
 	@Test
 	public void testCreateHamper() {
 		System.out.println("Test create hamper");
 		testHamper.createHamper();
-		if (testHamper.totalWholeGrains < requiredWholeGrains || testHamper.totalFruitVeggies < requiredFruitVeggies
-		|| testHamper.totalProtein < requiredProtein || testHamper.totalOther < requiredOther || testHamper.totalCalories < requiredCalories) {
+		if (testHamper.getTotalWholeGrains() < requiredWholeGrains || testHamper.getTotalFruitVeggies() < requiredFruitVeggies
+		|| testHamper.getTotalProtein() < requiredProtein || testHamper.getTotalOther() < requiredOther || testHamper.getTotalCalories() < requiredCalories) {
 			assertEquals("createHamper did not meet the requirements: ", true, false);
 		}
 	}
 	
+	/*
+	 * This test makes sure that Hamper throws an IllegalArgumentException when it is passed invalid input
+	 */
+	@Test 
+	public void testHamperThrowsIllegalArgumentException() {
+		System.out.println("Test Hamper throws IllegalArgumentException");
+		boolean exceptionThrown = false;
+		try {
+			Hamper badHamper = new Hamper(-1, 0, 1, 2, 1);
+		}
+		catch (IllegalArgumentException e) {
+			exceptionThrown = true;
+		}
+		assertTrue("Hamper did not throw an exception when given invalid input", exceptionThrown);
+	}
+	
+	/*
+	 * This test makes sure that getFoddItems returns the expected FoodItems
+	 */
 	@Test
 	public void testGetFoodItems() {
 		System.out.println("Test get food items");
-		
-		FoodItem [] result = testHamper.getFoodItems();
+		ArrayList<FoodItem> result = testHamper.getFoodItems();
 		for (int i = 0; i < foodItems.length; i++) {
-			assertEquals("getFoodItems did not return the correct foodItem: ", result[i], foodItems[i]);
+			assertEquals("getFoodItems did not return the correct foodItem: ", result.get(i), foodItems[i]);
 		}
 	}
 	
+	/*
+	 * This test makes sure that ValidateHamper returns the correct value when the hamper is not done/valid
+	 */
 	@Test
-	public void testValidateHamper() {
-		System.out.println("Test validate hamper");
-		boolean result = testHamper.validateHamper();
-		assertEquals("validateHamper was incorrect: ", result, true);
+	public void testValidateHamperInvalid() {
+		System.out.println("Test invalid validate hamper");
+		Hamper validHamper = new Hamper(0, 0, 0, 0, 1);
+		validHamper.createHamper();
+		boolean result = validHamper.validateHamper();
+		assertEquals("validateHamper returned an incorrect value: ", result, false);
+	}
+	
+	/*
+	 * This test makes sure that Validate Hamper returns the correct value when the hamper is done
+	 */
+	@Test
+	public void testValidateHamperValid() {
+		System.out.println("Test valid validate hamper");
+		Hamper validHamper = new Hamper(1, 0, 0, 1, 1);
+		validHamper.createHamper();
+		boolean result = validHamper.validateHamper();
+		assertEquals("validateHamper returned an incorrect value: ", result, true);
 	}
 }
+
+
